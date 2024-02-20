3e64ff0ba04560c4d368894cd9b197085cb0ec8f
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-02-20 20:01:30 +0530

    added seo and assets

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import { Room } from "./Room";
 
 export default function Page() {
   return (
-    <Room>
-      <h1 className="font-extralight">hellow</h1>
-    </Room>
+    <h1 className="text-white">hellow</h1>
   );
 }
